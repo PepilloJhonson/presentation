a978bd1e5a5d6c31e7d969cc3b3319a89184770d
PepilloJhonson <josemamani.sis21@tecba.edu.bo>
2024-09-27 00:12:44 +0000

    se la comio

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hola mundo que loke</h1>
+      <h1>hola mundo, que chico payton se la coma entera</h1>
     </div>
   );
 }
